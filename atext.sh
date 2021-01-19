786f9b321b092f4b4da31872123c809c413c3e60
akk16 <arunkk@eng.ucsd.edu>
2021-01-19 15:28:18 -0800

    anchors

--- atext.sh
+++ atext.sh
@@ -1,3 +1,3 @@
-sed -i -- 's/<h2>Members<\/h2>/<br\/><a name="members"><h2>Members<\/h2><\/a>/g' index.html
-sed -i -- 's/<h2>Sponsors<\/h2>/<br\/><a name="sponsors"><h2>Sponsors<\/h2><\/a>/g' index.html
+sed -i -- 's/<h2>Members<\/h2>/<a name="members"><h2>Members<\/h2><\/a>/g' index.html
+sed -i -- 's/<h2>Sponsors<\/h2>/<a name="sponsors"><h2>Sponsors<\/h2><\/a>/g' index.html
 
